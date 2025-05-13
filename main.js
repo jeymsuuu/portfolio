4518615a128fbdde4a332260f3cb360f8d30e81c
eya <mharla.khaikhea@gmail.com>
2025-05-13 22:36:35 +0800

    i made some changes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["STI Student", "BSIT student"],
+    strings: ["STI Student", "BSIT student", "Future Web Developer", "Future Game Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
